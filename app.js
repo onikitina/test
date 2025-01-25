87318f338f1775f5fb51d898422c0c2e7d5b7c77
onikitina <nikitina.olg@gmail.com>
2025-01-26 00:14:02 +0500

    Commit for Support number 1

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+
+let firstname = 'Olga';
+let lastname = 'Nikitina';
+let age = '40';
+let city = 'Tashkent';
+let hobby = 'Biology';
+console.log('Привет поддержке от ', firstname);
